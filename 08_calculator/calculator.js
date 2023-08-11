191e76d703a111644ef325ead58a55c09619fb87
safetyWhat <137813061+safetyWhat@users.noreply.github.com>
2023-08-11 06:06:01 -0400

    Complete the calculator excercise.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	let solution = a + b;
+  return solution;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	let solution = a - b;
+  return solution;
 };
 
-const sum = function() {
-	
+const sum = function(number) {
+  let arr = number;
+	let solution = arr.reduce((sum, current) => sum + current, 0);
+  return solution;
 };
 
-const multiply = function() {
-
+const multiply = function(number) {
+  let arr = number;
+	let solution = arr.reduce((prod, current) => prod * current);
+  return solution;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let solution = Math.pow(a, b);
+  return solution;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) { //n! = (n - 1)! * n
+  if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
