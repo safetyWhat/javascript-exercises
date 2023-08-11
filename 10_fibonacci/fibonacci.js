fd45c6acefc39f9efd6d740f080eff49c99ef9e7
safetyWhat <137813061+safetyWhat@users.noreply.github.com>
2023-08-11 16:05:59 -0400

    Complete fibonacci exercise.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(arrLength) {
+    if (arrLength < 0) {
+        return 'OOPS';
+    }
+    let a = 0;
+    let b = 1;
+    let fib = [0, 1];
+    for (x = 2; x <= arrLength; ++x) {
+        let y = a + b;
+        fib.push(y);
+        a = b;
+        b = y;
+        //console.log(a,b,fib)
+    }
+    return fib[arrLength]
 };
 
 // Do not edit below this line
