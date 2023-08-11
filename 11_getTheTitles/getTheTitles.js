8bbdcec89a9383e494a6b77cb3c3e99e3484ac9c
safetyWhat <137813061+safetyWhat@users.noreply.github.com>
2023-08-11 16:33:42 -0400

    Complete get the titles.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let titles = [];
+    titles.push(books[0].title);
+    titles.push(books[1].title);
+    return titles;
 };
 
 // Do not edit below this line
