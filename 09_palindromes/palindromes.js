fbf0dd0d1400c42ff2f0b32b606cc81971bb9882
safetyWhat <137813061+safetyWhat@users.noreply.github.com>
2023-08-11 07:11:03 -0400

    Complete palindrome exercise.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (a) {
+    //let pal = a;
 
+    let pal = a.replace(/[^\w\s\']|_/g, "") //Remove special characters.
+         .replace(/\s+/g, "")//Remove extra spaces.
+         .toLowerCase(); //Convert to all lower case letters.
+    console.log(pal)
+
+    function reverseString(str) {
+        let splitString = str.split('');
+        let reverseArray = splitString.reverse();
+        let joinArray = reverseArray.join('');
+        return joinArray;
+    }
+    if (pal === reverseString(pal)) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
