263c5862b974e7824f932f1e39251a2f18ca4d4a
safetyWhat <137813061+safetyWhat@users.noreply.github.com>
2023-08-12 22:12:31 -0400

    Complete Find the oldest.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function (array) {
+    return array.reduce((thatPerson, thisPerson) => {
+      const thatAge = getAge(thatPerson.yearOfBirth, thatPerson.yearOfDeath);
+      const thisAge = getAge(
+        thisPerson.yearOfBirth,
+        thisPerson.yearOfDeath
+      );
+      return thatAge < thisAge ? thisPerson : thatPerson;
+    });
+  };
+  
+  const getAge = function (birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  };
 
 // Do not edit below this line
 module.exports = findTheOldest;
